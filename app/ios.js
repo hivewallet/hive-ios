f618ca89d566e7a5e24e40a7308b223a831feca2
Ben Jennings <ben@idealogue.io>
2014-07-03 17:19:31 +1000

    Add test js for clipboard

--- app/ios.js
+++ app/ios.js
@@ -1,5 +1,8 @@
 if(window.hasOwnProperty('cordova')) {
   document.addEventListener('deviceready', function() {
     navigator.splashscreen.hide()
+    var text = "Hello World!";
+    cordova.plugins.clipboard.copy(text);
+    cordova.plugins.clipboard.paste(function (text) { alert(text); });
   }, false)
 }
