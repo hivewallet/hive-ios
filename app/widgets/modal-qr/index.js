a5f10e4b6fb68617ba15a78f076d5e66489c3e9c
haustraliaer <davidvonhauser@gmail.com>
2014-07-29 17:18:37 +1000

    Move copy to clipboard alert into ractive

--- app/widgets/modal-qr/index.js
+++ app/widgets/modal-qr/index.js
@@ -7,12 +7,15 @@ var getNetwork = require('hive-network')
 
 module.exports = function showTooltip(data){
 
+  var ractiveData = data
+  ractiveData.clipboard_msg = 'Copy to clipboard'
+
   var ractive = new Ractive({
     el: document.getElementById('tooltip'),
     partials: {
       content: require('./content.ract').template,
     },
-    data: data
+    data: ractiveData
   })
 
   var canvas = ractive.nodes['qr-canvas']
@@ -25,7 +28,10 @@ module.exports = function showTooltip(data){
 
   ractive.on('copy', function(){
     cordova.plugins.clipboard.copy(ractive.get('address'));
-    alert('Copied to clipboard');
+    ractive.set('clipboard_msg', 'Address copied!')
+    setTimeout(function(){
+      ractive.set('clipboard_msg', 'Copy to clipboard')
+    }, 1000)
   })
 
   return ractive
