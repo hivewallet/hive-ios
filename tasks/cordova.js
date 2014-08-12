9a320f3816149c04d7922a4b56057ee95ea2b69b
haustraliaer <davidvonhauser@gmail.com>
2014-08-12 15:08:42 +1000

    Add config task to cordova

--- tasks/cordova.js
+++ tasks/cordova.js
@@ -1,7 +1,32 @@
+var fs = require('fs')
 var copy = require('./util').copy
 
-function cordova(callback) {
+function cordovaCopy(callback) {
   copy('./build/', './cordova/www/', callback)
 }
 
-module.exports = cordova
+function cordovaConfig(callback) {
+  return function(){
+    var filename = './cordova/config_template.xml'
+    var newfile = './cordova/config.xml'
+    fs.readFile(filename, { encoding: 'utf8' }, function(err, index){
+      if(err) return callback(err);
+
+        var accessOpen = '<access origin="'
+        var accessClose = '" />'
+        var hostWhiteLists = [
+          accessOpen + process.env.DB_HOST + accessClose,
+          accessOpen + process.env.HOST + accessClose,
+          accessOpen + process.env.PROXY_URL + accessClose,
+          "</widget>"
+        ].join("\n")
+
+      fs.writeFile(newfile, index.replace('</widget>', hostWhiteLists), callback)
+    })
+  }
+}
+
+module.exports = {
+  copy: cordovaCopy,
+  config: cordovaConfig
+}
