32e4e2100069e2888bf3b27d5b6923abfdad3467
haustraliaer <davidvonhauser@gmail.com>
2014-08-12 15:00:05 +1000

    Remove comments

--- tasks/cordova.js
+++ tasks/cordova.js
@@ -4,22 +4,4 @@ function cordova(callback) {
   copy('./build/', './cordova/www/', callback)
 }
 
-// config task...
-
-// gulp.task('cordova-config', function(){
-//   var accessOpen = '<access origin="'
-//   var accessClose = '" />'
-//   var hostWhiteLists = [
-//     accessOpen + process.env.DB_HOST + accessClose,
-//     accessOpen + process.env.HOST + accessClose,
-//     accessOpen + process.env.PROXY_URL + accessClose,
-//     "</widget>"
-//   ].join("\n")
-
-//   gulp.src('./cordova/config_template.xml')
-//     .pipe(replace('</widget>', hostWhiteLists))
-//     .pipe(rename('./config.xml'))
-//     .pipe(gulp.dest('./cordova/'));
-// })
-
 module.exports = cordova
