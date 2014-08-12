db63d87039c52329b5c2790b4d4be5ea262b11bc
haustraliaer <davidvonhauser@gmail.com>
2014-08-12 15:11:01 +1000

    Remove wrapping function

--- tasks/cordova.js
+++ tasks/cordova.js
@@ -6,24 +6,22 @@ function cordovaCopy(callback) {
 }
 
 function cordovaConfig(callback) {
-  return function(){
-    var filename = './cordova/config_template.xml'
-    var newfile = './cordova/config.xml'
-    fs.readFile(filename, { encoding: 'utf8' }, function(err, index){
-      if(err) return callback(err);
+  var filename = './cordova/config_template.xml'
+  var newfile = './cordova/config.xml'
+  fs.readFile(filename, { encoding: 'utf8' }, function(err, index){
+    if(err) return callback(err);
 
-        var accessOpen = '<access origin="'
-        var accessClose = '" />'
-        var hostWhiteLists = [
-          accessOpen + process.env.DB_HOST + accessClose,
-          accessOpen + process.env.HOST + accessClose,
-          accessOpen + process.env.PROXY_URL + accessClose,
-          "</widget>"
-        ].join("\n")
+      var accessOpen = '<access origin="'
+      var accessClose = '" />'
+      var hostWhiteLists = [
+        accessOpen + process.env.DB_HOST + accessClose,
+        accessOpen + process.env.HOST + accessClose,
+        accessOpen + process.env.PROXY_URL + accessClose,
+        "</widget>"
+      ].join("\n")
 
-      fs.writeFile(newfile, index.replace('</widget>', hostWhiteLists), callback)
-    })
-  }
+    fs.writeFile(newfile, index.replace('</widget>', hostWhiteLists), callback)
+  })
 }
 
 module.exports = {
